4f5d2ee177469e92b5dc2cd794f09172540a7534
f11m4at4 <f11m4at4@gmail.com>
2024-07-30 21:57:12 +0900

    CPlayer 클래스 컴포넌트 추가하기

--- Source/Shooting/Private/CPlayer.cpp
+++ Source/Shooting/Private/CPlayer.cpp
@@ -9,6 +9,26 @@ ACPlayer::ACPlayer()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// BodyMesh 그릇에 StaticMeshComponent 를 만들어서 담고 싶다.
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMesh"));
+	// BodyMesh 를 Root 로 등록하고 싶다.
+	RootComponent = BodyMesh;
+
+	SpaceShipMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SpaceShipMesh"));
+	SpaceShipMesh->SetupAttachment(BodyMesh);
+
+	// 사용할 Static Mesh 데이터를 읽어들여서 컴포넌트에 할당하고싶다.
+	ConstructorHelpers::FObjectFinder<UStaticMesh> TempMesh(TEXT("/Script/Engine.StaticMesh'/Game/SpaceShip/Spaceship_ARA.Spaceship_ARA'"));
+	// 로딩이 성공하면
+	if(TempMesh.Succeeded())
+	{
+		// -> SpaceShipMesh 에 사용할 스태틱메시로 TempMesh 의 값을 넣어주고 싶다.
+		SpaceShipMesh->SetStaticMesh(TempMesh.Object);
+		// 위치값을 세팅하고 싶다.
+		SpaceShipMesh->SetRelativeRotation(FRotator(0, 90, 90));
+		// 크기값을 세팅하고 싶다.
+		SpaceShipMesh->SetRelativeScale3D(FVector(6));
+	}
 }
  
 // Called when the game starts or when spawned
--- Source/Shooting/Public/CPlayer.h
+++ Source/Shooting/Public/CPlayer.h
@@ -26,4 +26,9 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+public:
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* BodyMesh;
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* SpaceShipMesh;
 };
